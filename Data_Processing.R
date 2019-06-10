a34fdd45605ae6eacf9ad5895aa0d5ac0aa62077
zhaohehe <zhaohehe520@gmail.com>
2019-06-10 11:28:02 -0400

    update

--- Data_Processing.R
+++ Data_Processing.R
@@ -18,3 +18,60 @@ ggplot(train, aes(x=Pclass,fill = factor(Survived))) +
   xlab("Pcclass") +
   ylab("Total Count")+
   labs(fill = "Survived")
+
+head(as.character(train$Name))
+length(unique(data.combined$Name))
+dup.names <- as.character(data.combined[which(duplicated(as.character(data.combined$Name))),"Name"])
+data.combined[which(data.combined$Name %in% dup.names),]
+
+library(stringr)
+misses <- data.combined[which(str_detect(data.combined$Name,"Miss.")),]
+males <- data.combined[which(train$Sex == "male"),]
+
+extractTitle <- function(name){
+  name <- as.character(name)
+  
+  if (length(grep("Miss.",name)) > 0){
+    return("Miss.")
+  }else if (length(grep("Master.",name)) > 0){
+    return("Master.")
+  }else if (length(grep("Mrs.",name)) > 0){
+    return("Mrs.")
+  }else if (length(grep("Mr.",name)) > 0){
+    return("Mr.")
+  }else{
+    return("Other")
+  }
+}
+  
+titles <- NULL
+for (i in 1:nrow(data.combined)) {
+  titles <- c(titles, extractTitle(data.combined[i,"Name"]))
+}
+data.combined$title <- as.factor(titles)
+
+ggplot(data.combined[1:891,],aes(x = title, fill = Survived)) +
+         geom_bar(width = 0.5)+
+         facet_wrap(~Pclass)+
+         ggtitle("Pclass")+
+         xlab("Title")+
+         ylab("Total Count")+
+         labs(fill = "Survived")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
